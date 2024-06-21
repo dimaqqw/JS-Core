f7abed422eede00a922dd40c90fa5687d9f1eb46
dimaqqw <dimaaqqw@mail.ru>
2024-06-21 13:32:27 +0300

    complete arrays module

---
+++ [4]arrays/answer.js
@@ -0,0 +1,8 @@
+function minInArray(arr) {
+  return Math.min(...arr)
+}
+
+// Tests
+console.log(minInArray([1, 2, 3, 4, 5]))
+console.log(minInArray([5, 4, 3, 2, 1, 0]))
+console.log(minInArray([5, 4, 3, 4, 5]))
